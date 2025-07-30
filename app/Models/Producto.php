c76651415fd194f2ae1b27209fe0a3e4100ec205
Susana Martinez Tamayo <sm2611@hotmail.com>
2025-07-30 13:52:59 -0600

    actualizar modelos, error en producción

--- app/Models/Producto.php
+++ app/Models/Producto.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\factories\HasFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Producto extends Model
