e05f8289b14185d60ecfd045b04e3dd50395ee68
Susana Martinez Tamayo <sm2611@hotmail.com>
2026-04-28 17:50:26 -0600

    google buscador

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,15 +4,30 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  
+
   <link rel="shortcut icon" href="/img/logo.png" type="image/png"/>
-  
-  <title>Jeax Rines</title>
-  
+
+  <title>{{ $seo['title'] ?? 'JEAX Store' }}</title>
+  <meta name="description" content="{{ $seo['description'] ?? '' }}">
+  <link rel="canonical" href="{{ $seo['url'] ?? url()->current() }}">
+
   @vite(['resources/js/app.js'])
 </head>
 
 <body>
-    <div id ="root"></div>
+  
+    <!-- CONTENIDO SEO PARA GOOGLE -->
+    <div style="padding:20px; max-width:900px; margin:auto;">
+        <h1>{{ $seo['title'] ?? 'JEAX Store' }}</h1>
+        <p>{{ $seo['description'] ?? '' }}</p>
+
+        @isset($content)
+            {!! $content !!}
+        @endisset
+    </div>
+
+    <!-- TU APP REACT -->
+    <div id="root"></div>
+
 </body>
 </html>
